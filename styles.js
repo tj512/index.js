bb1f7ef5452ef0079304d442cfb54062ed60eef9
tj512 <67591802+tj512@users.noreply.github.com>
2025-08-11 19:33:49 +0800

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  }
+});
